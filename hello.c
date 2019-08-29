88d87945cbef0da2b752fe4f73ed47815591df65
Connor Pumilia <pumiliaconnor@gmail.com>
2019-08-29 15:44:52 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+    *Author: Connor Pumilia
+    *Date: Aug. 29th 2019
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+
+    printf("Hello World!\n");
+    return 0;
+}
