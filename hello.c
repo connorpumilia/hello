ded45cb50a8169490cedf8910c9b1a9d3f2aa3df
Connor Pumilia <pumiliaconnor@gmail.com>
2019-08-29 17:33:03 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
-    *Author: Connor Pumilia
-    *Date: Aug. 29th 2019
-
-
+ /**
+  *Author: Connor Pumilia
+  *Date: Aug. 29th 2019
+  *
+  * A simple hello world program in C
+  *
+  */
 #include <stdlib.h>
 #include <stdio.h>
 
